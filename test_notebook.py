815a80333316ece30e41468a22d56b10cf982f75
unknown <sayan2019@aol.com>
2019-11-03 08:28:03 -0800

    adds a python script and push it to Github

---
+++ test_notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2>This notebook will be mainly used for the capstone project.</h2>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
